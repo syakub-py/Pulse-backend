b21c8db2a5e347f3ea2e644071bfd581ca48afcd
Sam <syakubov101@gmail.com>
2024-07-31 16:39:04 -0400

    add logging to session

--- DB/ORM/Utils/Session.py
+++ DB/ORM/Utils/Session.py
@@ -3,17 +3,21 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from DB.ORM.Config import DATABASE_URL
 from contextlib import contextmanager
 from DB.ORM.Base import Base
+from LoggerConfig import logger
 
 @contextmanager
 def session_scope():
     engine = create_engine(DATABASE_URL)
+    logger.info("Connected to database")
     session = scoped_session(sessionmaker(bind=engine))
+    logger.info("Session created successfully")
     Base.metadata.create_all(engine)
+    logger.info("All Tables created successfully")
     try:
         yield session
         session.commit()
     except Exception as e:
         session.rollback()
-        raise e
+        logger.error(e)
     finally:
         session.remove()
