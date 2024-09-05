80d840e9d85f628705e76cc9b9e22d9098da6821
atamayev <75034340+atamayev@users.noreply.github.com>
2024-09-05 04:47:30 +0300

    Update SocketConnection.py

--- SocketConnection.py
+++ SocketConnection.py
@@ -7,7 +7,6 @@ active_users: set[str] = set()
 def get_active_users() -> set[str]:
     return active_users
 
-
 @router.websocket("/ws/")
 async def init_websocket_connection(websocket: WebSocket, token: str) -> None:
     try:
@@ -20,7 +19,6 @@ async def init_websocket_connection(websocket: WebSocket, token: str) -> None:
         active_users.remove(token)
         print(f"{token} disconnected. Active users: {list(active_users)}")
         return None
-
 
 def isUserActive(uid: int) -> bool:
     return uid in active_users
